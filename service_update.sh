7825a213c74fae3fe09fdf39c854322fb66d82b5
moonlightming <605365071@qq.com>
2021-04-16 16:51:01 +0800

    修改模板

--- service_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker pull registry.cn-shenzhen.aliyuncs.com/moonlightming/xblog-hugo:latest
-
-docker service update --image registry.cn-shenzhen.aliyuncs.com/moonlightming/xblog-hugo:latest hugo_xblog-hugo --with-registry-auth
